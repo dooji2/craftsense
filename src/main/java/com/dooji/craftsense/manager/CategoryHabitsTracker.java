bbfad41edd9c58e1cdfc8dab11cffb3dab060540
dooji2 <cmir79411@gmail.com>
2024-11-06 19:48:47 +0200

    Add item tracking

--- src/main/java/com/dooji/craftsense/manager/CategoryHabitsTracker.java
+++ src/main/java/com/dooji/craftsense/manager/CategoryHabitsTracker.java
@@ -2,6 +2,7 @@ package com.dooji.craftsense.manager;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.FileReader;
@@ -16,11 +17,22 @@ public class CategoryHabitsTracker {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static final Path HABITS_PATH = Path.of("config/CraftSense/habits.json");
 
-    private Map<String, Integer> categoryCraftCounts;
+    private static CategoryHabitsTracker instance;
+
+    public Map<String, Integer> categoryCraftCount;
+    public Map<String, Integer> itemCraftCount;
 
     public CategoryHabitsTracker() {
-        categoryCraftCounts = new HashMap<>();
+        categoryCraftCount = new HashMap<>();
+        itemCraftCount = new HashMap<>();
         load();
+    }
+
+    public static CategoryHabitsTracker getInstance() {
+        if (instance == null) {
+            instance = new CategoryHabitsTracker();
+        }
+        return instance;
     }
 
     private void load() {
@@ -28,7 +40,22 @@ public class CategoryHabitsTracker {
             Files.createDirectories(HABITS_PATH.getParent());
             if (Files.exists(HABITS_PATH)) {
                 try (FileReader reader = new FileReader(HABITS_PATH.toFile())) {
-                    categoryCraftCounts = GSON.fromJson(reader, new TypeToken<Map<String, Integer>>() {}.getType());
+                    Map<String, Map<String, Integer>> data = GSON.fromJson(reader, new TypeToken<Map<String, Map<String, Integer>>>() {}.getType());
+
+                    if (data != null && data.containsKey("categoryCraftCount") && data.containsKey("itemCraftCount")) {
+                        categoryCraftCount = data.get("categoryCraftCount");
+                        itemCraftCount = data.get("itemCraftCount");
+                    } else {
+                        categoryCraftCount = new HashMap<>();
+                        itemCraftCount = new HashMap<>();
+                    }
+                } catch (JsonSyntaxException e) {
+                    try (FileReader legacyReader = new FileReader(HABITS_PATH.toFile())) {
+                        categoryCraftCount = GSON.fromJson(legacyReader, new TypeToken<Map<String, Integer>>() {}.getType());
+                        itemCraftCount = new HashMap<>();
+                    } catch (IOException innerException) {
+                        innerException.printStackTrace();
+                    }
                 }
             } else {
                 save();
@@ -40,18 +67,26 @@ public class CategoryHabitsTracker {
 
     public void save() {
         try (FileWriter writer = new FileWriter(HABITS_PATH.toFile())) {
-            GSON.toJson(categoryCraftCounts, writer);
+            Map<String, Map<String, Integer>> data = new HashMap<>();
+            data.put("categoryCraftCount", categoryCraftCount);
+            data.put("itemCraftCount", itemCraftCount);
+            GSON.toJson(data, writer);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void recordCraft(String itemCategory) {
-        categoryCraftCounts.put(itemCategory, categoryCraftCounts.getOrDefault(itemCategory, 0) + 1);
+    public void recordCraft(String itemCategory, String itemName) {
+        categoryCraftCount.put(itemCategory, categoryCraftCount.getOrDefault(itemCategory, 0) + 1);
+        itemCraftCount.put(itemName, itemCraftCount.getOrDefault(itemName, 0) + 1);
         save();
     }
 
     public int getCraftCount(String category) {
-        return categoryCraftCounts.getOrDefault(category, 0);
+        return categoryCraftCount.getOrDefault(category, 0);
+    }
+
+    public int getItemCraftCount(String itemName) {
+        return itemCraftCount.getOrDefault(itemName, 0);
     }
 }
