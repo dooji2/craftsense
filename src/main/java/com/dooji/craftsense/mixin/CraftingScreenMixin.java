1934da67d84847ab7578e7e24d4c2642af676eb2
dooji2 <cmir79411@gmail.com>
2024-12-22 17:18:53 +0200

    Fix tooltip pos

--- src/main/java/com/dooji/craftsense/mixin/CraftingScreenMixin.java
+++ src/main/java/com/dooji/craftsense/mixin/CraftingScreenMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.CraftingScreen;
+import net.minecraft.client.gui.tooltip.TooltipPositioner;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.entity.player.PlayerInventory;
@@ -18,12 +19,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.*;
 import net.minecraft.screen.CraftingScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
+import org.joml.Vector2i;
+import org.joml.Vector2ic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -286,14 +290,15 @@ public abstract class CraftingScreenMixin {
 
     @Unique
     private void renderTooltip(DrawContext context, String title, String description, int x, int y) {
-        List<Text> tooltip = new ArrayList<>();
-        tooltip.add(Text.literal(title).formatted(Formatting.WHITE));
+        List<OrderedText> tooltip = new ArrayList<>();
+        tooltip.add(Text.literal(title).formatted(Formatting.WHITE).asOrderedText());
 
         String[] descriptionLines = description.split("\n");
         for (String line : descriptionLines) {
-            tooltip.add(Text.literal(line).formatted(Formatting.GRAY));
+            tooltip.add(Text.literal(line).formatted(Formatting.GRAY).asOrderedText());
         }
-        
-        context.drawTooltip(MinecraftClient.getInstance().textRenderer, tooltip, x + 20, y + 5);
+
+        TooltipPositioner fixedPositioner = (screenWidth, screenHeight, tooltipX, tooltipY, tooltipWidth, tooltipHeight) -> new Vector2i(tooltipX, tooltipY);
+        context.drawTooltip(MinecraftClient.getInstance().textRenderer, tooltip, fixedPositioner, x + 30, y - 7);
     }
 }
