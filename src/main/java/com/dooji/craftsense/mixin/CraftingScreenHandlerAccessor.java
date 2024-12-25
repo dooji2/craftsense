43f849b715ed47adfaae742978438f4c1a8368ca
dooji2 <cmir79411@gmail.com>
2024-12-25 19:02:29 +0200

    Change to CraftingInventory

--- src/main/java/com/dooji/craftsense/mixin/CraftingScreenHandlerAccessor.java
+++ src/main/java/com/dooji/craftsense/mixin/CraftingScreenHandlerAccessor.java
@@ -1,12 +1,13 @@
 package com.dooji.craftsense.mixin;
 
-import net.minecraft.inventory.RecipeInputInventory;
+import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.screen.CraftingScreenHandler;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(CraftingScreenHandler.class)
 public interface CraftingScreenHandlerAccessor {
     @Accessor("input")
-    RecipeInputInventory getInput();
+    CraftingInventory getInput();
 }
