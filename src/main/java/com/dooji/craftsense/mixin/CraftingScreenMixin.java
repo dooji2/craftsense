0ea696ca4a7c560efa9e52945c5c165e68b4d933
dooji2 <doojisbasement@gmail.com>
2025-01-01 16:30:12 +0200

    Add shift + click support and customizable keybind

--- src/main/java/com/dooji/craftsense/mixin/CraftingScreenMixin.java
+++ src/main/java/com/dooji/craftsense/mixin/CraftingScreenMixin.java
@@ -1,17 +1,21 @@
 package com.dooji.craftsense.mixin;
 
 import com.dooji.craftsense.CraftSense;
+import com.dooji.craftsense.CraftSenseKeyBindings;
 import com.dooji.craftsense.CraftingPredictor;
 import com.dooji.craftsense.manager.CategoryHabitsTracker;
+import com.dooji.craftsense.manager.CategoryManager;
 import com.dooji.craftsense.network.payloads.CraftItemPayload;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.CraftingScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.tooltip.TooltipPositioner;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.RecipeInputInventory;
 import net.minecraft.item.ItemStack;
@@ -27,7 +31,6 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 import org.joml.Vector2i;
-import org.joml.Vector2ic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -37,9 +40,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.*;
 
-import static com.dooji.craftsense.manager.CategoryManager.getCategory;
-
-@Mixin(CraftingScreen.class)
+@Mixin(HandledScreen.class)
 public abstract class CraftingScreenMixin {
 
     @Unique
@@ -65,12 +66,13 @@ public abstract class CraftingScreenMixin {
 
     @Inject(method = "render", at = @At("TAIL"))
     private void renderCraftingPrediction(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (!CraftSense.configManager.isEnabled()) {
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        if (!CraftSense.configManager.isEnabled() || !(client.currentScreen instanceof CraftingScreen)) {
             return;
         }
 
         CraftingScreen craftingScreen = (CraftingScreen) (Object) this;
-        MinecraftClient client = MinecraftClient.getInstance();
         PlayerInventory playerInventory = client.player.getInventory();
         World world = client.world;
 
@@ -196,6 +198,13 @@ public abstract class CraftingScreenMixin {
 
     @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
     private void onSuggestedRecipeClick(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        boolean isShiftPressed = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), InputUtil.GLFW_KEY_LEFT_SHIFT);
+
+        if (!CraftSense.configManager.isEnabled() || !(client.currentScreen instanceof CraftingScreen)) {
+            return;
+        }
+
         if (isMouseOverSlot((int) mouseX, (int) mouseY, resultSlotX, resultSlotY)) {
             if (showFirstTimeTooltips) {
                 progress++;
@@ -205,11 +214,12 @@ public abstract class CraftingScreenMixin {
                 }
             }
 
-            MinecraftClient client = MinecraftClient.getInstance();
             PlayerInventory playerInventory = client.player.getInventory();
             World world = client.world;
 
             CraftingScreenHandler handler = ((CraftingScreen) (Object) this).getScreenHandler();
+            ItemStack cursorStack = handler.getCursorStack();
+
             RecipeInputInventory input = ((CraftingScreenHandlerAccessor) handler).getInput();
             CraftingPredictor predictor = CraftingPredictor.getInstance(world.getRecipeManager());
             Optional<CraftingRecipe> lastCraftedRecipe = predictor.suggestLastCraftedItem(input, playerInventory, handler.getCursorStack(), world);
@@ -223,28 +233,68 @@ public abstract class CraftingScreenMixin {
 
                 if (recipeId != null) {
                     ItemStack resultStack = recipe.getOutput(world.getRegistryManager()).copy();
-                    ItemStack cursorStack = handler.getCursorStack();
 
-                    if (cursorStack.isEmpty()) {
-                        handler.setCursorStack(resultStack);
-                    } else if (areStacksEqualWithComponents(cursorStack, resultStack) && resultStack.isStackable()) {
-                        cursorStack.increment(resultStack.getCount());
-                        handler.setCursorStack(cursorStack);
-                    } else {
+                    if (!cursorStack.isEmpty() && (!cursorStack.isStackable() || !areStacksEqualWithComponents(cursorStack, resultStack))) {
                         return;
                     }
 
                     CategoryHabitsTracker habitsConfig = CategoryHabitsTracker.getInstance();
-                    String category = getCategory(resultStack.getItem());
+                    String category = CategoryManager.getCategory(resultStack.getItem());
                     habitsConfig.recordCraft(category, resultStack.getItem().getTranslationKey());
 
                     Identifier channelId = new Identifier("craftsense", "craft_item");
-                    PacketByteBuf packetBuffer = CraftItemPayload.createPacket(recipeId.toString());
+                    PacketByteBuf packetBuffer = CraftItemPayload.createPacket(recipeId.toString(), isShiftPressed);
 
                     ClientPlayNetworking.send(channelId, packetBuffer);
 
                     cir.setReturnValue(true);
                 }
+            }
+        }
+    }
+
+    @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
+    private void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        if (CraftSenseKeyBindings.quickCraftKey.matchesKey(keyCode, scanCode)) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            World world = client.world;
+
+            if (CraftSense.configManager.isEnabled() && client.player != null && client.world != null && client.currentScreen instanceof CraftingScreen) {
+                CraftingScreenHandler handler = ((CraftingScreen) (Object) this).getScreenHandler();
+                PlayerInventory inventory = client.player.getInventory();
+                RecipeInputInventory input = ((CraftingScreenHandlerAccessor) handler).getInput();
+
+                ItemStack cursorStack = handler.getCursorStack();
+                CraftingPredictor predictor = CraftingPredictor.getInstance(world.getRecipeManager());
+                String currentStateHash = predictor.calculateInputHash(input, inventory, cursorStack);
+
+                if (!currentStateHash.equals(lastGridHash)) {
+                    lastGridHash = currentStateHash;
+                    cachedLastCraftedRecipe = predictor.suggestLastCraftedItem(input, inventory, cursorStack, client.world);
+
+                    if (cachedLastCraftedRecipe.isEmpty()) {
+                        cachedSuggestedRecipe = predictor.suggestRecipe(input, inventory, cursorStack, client.world);
+                    } else {
+                        cachedSuggestedRecipe = Optional.empty();
+                    }
+                }
+
+                Optional<CraftingRecipe> recipe = cachedSuggestedRecipe.isPresent() ? cachedSuggestedRecipe : cachedLastCraftedRecipe;
+                recipe.ifPresent(r -> {
+                    Identifier recipeId = findRecipeId(world.getRecipeManager(), r);
+                    if (recipeId != null) {
+                        ItemStack resultStack = r.getOutput(world.getRegistryManager()).copy();
+                        String category = CategoryManager.getCategory(resultStack.getItem());
+                        CategoryHabitsTracker habitsConfig = CategoryHabitsTracker.getInstance();
+                        habitsConfig.recordCraft(category, resultStack.getItem().getTranslationKey());
+
+                        Identifier channelId = new Identifier("craftsense", "craft_item");
+                        PacketByteBuf packetBuffer = CraftItemPayload.createPacket(recipeId.toString(), true);
+
+                        ClientPlayNetworking.send(channelId, packetBuffer);
+                    }
+                });
+                cir.setReturnValue(true);
             }
         }
     }
