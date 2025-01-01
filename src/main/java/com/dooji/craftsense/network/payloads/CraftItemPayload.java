b3009e01ef42b415bb5950d1c1b4ec5ba0c361f9
dooji2 <doojisbasement@gmail.com>
2025-01-01 16:29:14 +0200

    Update payload

--- src/main/java/com/dooji/craftsense/network/payloads/CraftItemPayload.java
+++ src/main/java/com/dooji/craftsense/network/payloads/CraftItemPayload.java
@@ -3,19 +3,20 @@ package com.dooji.craftsense.network.payloads;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.network.PacketByteBuf;
 
-public record CraftItemPayload(String recipeId) {
+public record CraftItemPayload(String recipeId, Boolean isShiftPressed) {
 
     public static CraftItemPayload read(PacketByteBuf buf) {
-        return new CraftItemPayload(buf.readString());
+        return new CraftItemPayload(buf.readString(), buf.readBoolean());
     }
 
     public static void write(PacketByteBuf buf, CraftItemPayload payload) {
         buf.writeString(payload.recipeId());
+        buf.writeBoolean(payload.isShiftPressed());
     }
 
-    public static PacketByteBuf createPacket(String recipeId) {
+    public static PacketByteBuf createPacket(String recipeId, Boolean isShiftPressed) {
         PacketByteBuf buf = PacketByteBufs.create();
-        write(buf, new CraftItemPayload(recipeId));
+        write(buf, new CraftItemPayload(recipeId, isShiftPressed));
         return buf;
     }
 }
