1299e5ae2f6c2b6ead950a6aa373cd6f6840f7b6
dooji2 <cmir79411@gmail.com>
2025-01-01 13:38:38 +0200

    Update payload

--- src/main/java/com/dooji/craftsense/network/payloads/CraftItemPayload.java
+++ src/main/java/com/dooji/craftsense/network/payloads/CraftItemPayload.java
@@ -5,32 +5,23 @@ import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 import net.minecraft.network.codec.PacketCodec;
 
-public class CraftItemPayload implements CustomPayload {
-    public static final CustomPayload.Id<CraftItemPayload> ID = new CustomPayload.Id<>(Identifier.of("craftsense", "craft_item"));
-    public static final PacketCodec<PacketByteBuf, CraftItemPayload> CODEC = new PacketCodec<PacketByteBuf, CraftItemPayload>() {
+public record CraftItemPayload(String recipeId, boolean isShiftPressed) implements CustomPayload {
+    public static final Id<CraftItemPayload> ID = new Id<>(Identifier.of("craftsense", "craft_item"));
+    public static final PacketCodec<PacketByteBuf, CraftItemPayload> CODEC = new PacketCodec<>() {
         @Override
         public CraftItemPayload decode(PacketByteBuf buf) {
-            return new CraftItemPayload(buf.readString());
+            return new CraftItemPayload(buf.readString(), buf.readBoolean());
         }
-    
+
         @Override
         public void encode(PacketByteBuf buf, CraftItemPayload payload) {
-            buf.writeString(payload.getRecipeId());
+            buf.writeString(payload.recipeId());
+            buf.writeBoolean(payload.isShiftPressed());
         }
-    };    
-
-    private final String recipeId;
-
-    public CraftItemPayload(String recipeId) {
-        this.recipeId = recipeId;
-    }
-
-    public String getRecipeId() {
-        return recipeId;
-    }
+    };
 
     @Override
-    public CustomPayload.Id<? extends CustomPayload> getId() {
+    public Id<? extends CustomPayload> getId() {
         return ID;
     }
 }
