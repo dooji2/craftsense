4fedf39bcb2b2cab40041a31e91c29bf289812fa
dooji2 <doojisbasement@gmail.com>
2025-02-14 22:39:26 +0200

    lets not invert args - fixed updating wrong slot

--- src/main/java/com/dooji/craftsense/network/CraftSenseNetworking.java
+++ src/main/java/com/dooji/craftsense/network/CraftSenseNetworking.java
@@ -184,9 +184,7 @@ public class CraftSenseNetworking {
                 continue;
             }
 
-            player.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(
-                    handler.syncId, i + 1, 0, currentStack
-            ));
+            player.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(handler.syncId, 0, i + 1, currentStack));
         }
     }
 
