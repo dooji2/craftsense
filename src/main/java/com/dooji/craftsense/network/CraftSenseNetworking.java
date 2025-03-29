efe6682c9b33c8f3f227d13879d1fea33fb97bda
dooji2 <doojisbasement@gmail.com>
2025-03-29 15:49:04 +0200

    do not modify the output slot

--- src/main/java/com/dooji/craftsense/network/CraftSenseNetworking.java
+++ src/main/java/com/dooji/craftsense/network/CraftSenseNetworking.java
@@ -78,11 +78,12 @@ public class CraftSenseNetworking {
                     } else {
                         if (cursorStack.isEmpty()) {
                             handler.setCursorStack(resultStack);
-                            sendSlotUpdate(player, handler.syncId, -1, resultStack);
+                            // why was i updating the output slot um
+                            // sendSlotUpdate(player, handler.syncId, -1, resultStack);
                         } else if (areStacksEqualWithComponents(cursorStack, resultStack)) {
                             cursorStack.increment(resultStack.getCount());
                             handler.setCursorStack(cursorStack);
-                            sendSlotUpdate(player, handler.syncId, -1, cursorStack);
+                            // sendSlotUpdate(player, handler.syncId, -1, cursorStack);
                         } else {
                             return;
                         }
